f93a86fd2f9f1f4f3c5654dd7f544fbed4bc1899
feelswhiteman <96499344+feelswhiteman@users.noreply.github.com>
2022-05-18 02:00:29 +0300

    Add files via upload

--- scripts/kdeconnect.sh
+++ scripts/kdeconnect.sh
@@ -8,15 +8,15 @@ case "$1" in
 		for file in ${files//|/ }; do
 			kdeconnect-cli -d $(kdeconnect-cli -a --id-only) --share $file
 		done
-	;;
+		;;
 	-s|--screenshot) 
 		rm /tmp/tmp.png
 		flameshot gui -p /tmp/tmp.png
 		kdeconnect-cli -d $(kdeconnect-cli -a --id-only) --share /tmp/tmp.png
-	;;
+		;;
 	-c|--clipboard) 
 		kdeconnect-cli -d $(kdeconnect-cli -a --id-only) --share-text "$(xclip -o)"
-	;;
+		;;
     *) echo " Wrong argument.
     -f|--file: send file
     -s|--screenshot: send screenshot
